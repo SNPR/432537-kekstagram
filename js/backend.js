461fe4d5ae7cd78ae8939b848f434ccb744452f7
snpr <vaugk13@gmail.com>
2018-02-18 23:00:17 +0500

    Написал функцию, принимающую данные с сервера.

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,35 @@
 'use strict';
 
 (function () {
+  /**
+   * Отправляет запрос на сервер.
+   * @param {Function} onLoad Callback-функция, запускаемвая в случае удачной загрузки.
+   * @param {Function} onError Callback-функция, запускаемвая в случае неудачной загрузки.
+   */
   window.load = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
+    var URL_GET = 'https://js.dump.academy/kekstagram/data';
+    var TIMEOUT = 10000;
+    xhr.responseType = 'json';
+    xhr.timeout = TIMEOUT;
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('GET', URL_GET);
+    xhr.send();
   };
 })();
